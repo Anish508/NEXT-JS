3705e799e40758b35dbc1a83c9c42adb0f5bf957
Anish508 <anishbarke9741@gmail.com>
2025-08-20 16:42:28 +0530

    Understanding the concept of useSearchParams, UsePathname, useRouter hooks

--- basic-conepts/src/app/profile/page.tsx
+++ basic-conepts/src/app/profile/page.tsx
@@ -1,6 +1,27 @@
-"use client"
+"use client";
 
-function profile(){
-      return <h1>Profile component- client</h1>
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+function Profile() {
+  const router = useRouter();
+  const pathName = usePathname()
+  const searchParams = useSearchParams()
+  console.log(pathName);
+  
+  console.log(router);
+  console.log(searchParams.get("name"));
+  
+
+  const handleNavigate = () => {
+    router.push("/");
+  };
+
+  return (
+    <div>
+      <h1>Profile component</h1>
+      <button onClick={handleNavigate}>Navigate to home page</button>
+    </div>
+  );
 }
-export default profile
+
+export default Profile;
