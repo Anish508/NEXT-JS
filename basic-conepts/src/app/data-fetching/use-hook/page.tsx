6622fe013415a2e80d29228d996a6dabadb914b1
Anish508 <anishbarke9741@gmail.com>
2025-08-25 17:19:09 +0530

    Data fetching on server component by useing use hook and suspense fallback

---
+++ basic-conepts/src/app/data-fetching/use-hook/page.tsx
@@ -0,0 +1,47 @@
+import { Suspense } from "react";
+import { use } from "react";
+
+interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  gender: string;
+}
+
+interface userResponse {
+  users: User[];
+  total: number;
+}
+
+function getUser(): Promise<userResponse> {
+  return fetch("https://dummyjson.com/users").then((res) => res.json());
+}
+
+export default function UseHookExample() {
+  const userPromise = getUser();
+
+  return (
+    <div>
+      <h1>Use Hook Example</h1>
+      <Suspense fallback={<div>Loading...</div>}>
+        <UserList usersPromise={userPromise} />
+      </Suspense>
+    </div>
+  );
+}
+
+function UserList({ usersPromise }: { usersPromise: Promise<userResponse> }) {
+  const getUsersList = use(usersPromise);
+
+  return (
+    <div>
+      {getUsersList.users.map((user) => (
+        <div key={user.id}>
+          <p>{user.firstName}</p>
+          <p>{user.lastName}</p>
+          <p>{user.gender}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
